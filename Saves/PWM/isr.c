cf17c33294ba1cbd415b2e26ff9a104c952aa3ee
PastaLover631 <pastalover631@gmail.com>
2018-04-28 14:11:55 -0400

    Add files via upload

---
+++ Saves/PWM/isr.c
@@ -0,0 +1,82 @@
+/***********************************************************
+    Project:    Semester Project
+    Company:    CPE 490 Embedded Systems
+    Author:     Andrew Davies & John Bugay
+    File:       isr.c
+    Purpose:    holds interrupt service routines
+***********************************************************/
+//**Includes**//
+#include "isr.h"
+#include "address_map.h"
+#include "pwm.h"
+
+//**Global Valiables**//
+extern int ps2Interrupt;      //declared in main.c
+extern int encoderInterrupt;  //declared in main.c
+extern int monitorInterrupt;  //declared in main.c
+extern float percent;         //delcared in keypad.c
+
+static unsigned char END = 0; // Used to read the End-Of-Interrupt register to reset timer values
+
+//**Interrupt Service Routines**//
+void HPSTimer0ISR()   //half second period
+{
+    volatile int* HPSTimer0Ptr = (int*) HPS_TIMER0_BASE;
+    static char flag; //toggle flag
+  
+    ps2Interrupt = 1;
+    flag ++;
+    if(flag == 1)
+    {
+        encoderInterrupt = 1;
+    }
+    if(flag == 2)
+    {
+        monitorInterrupt = 1;
+        flag = 0;
+    }
+  
+    END = *(HPSTimer0Ptr + 3);  //resets HPSTimer0
+}
+
+void HPSTimer1ISR()
+{
+    volatile int* HPSTimer1Ptr = (int*) HPS_TIMER1_BASE;
+    volatile int* GPIOPtr = (int*)JP1_BASE;
+
+    static int pwmToggle = 0;
+
+    if(percent == 0)
+    {
+        *(GPIOPtr) = 0;
+        // *(GPIOPtr) &= 0xFFFFFFFD; // 1101
+    }
+    else if(percent == 1.0)
+    {
+        *(GPIOPtr) = 1;
+        // *(GPIOPtr) |= 0x2; // 0010
+    }
+    else
+    {
+        if(pwmToggle == 1)
+        {
+            *(GPIOPtr) = 1; // D0 = low
+            // *(GPIOPtr) |= 0x2;
+            SetPWM(1000, percent);
+
+            pwmToggle = 0;
+        }
+        else // pwmToggle == 0
+        {
+            *(GPIOPtr) = 0; // D0 = high
+            // *(GPIOPtr) &= 0xFFFFFFFD;
+            SetPWM( 1000, (1.0 - percent) );
+
+            pwmToggle = 1;
+        }
+    }
+
+    END = *(HPSTimer1Ptr + 3);
+}
+
+//**End of File**//
---
+++ Saves/PWM/main.c
@@ -0,0 +1,78 @@
+/***********************************************************
+	Project:	Semester Project
+	Company:	CPE 490 Embedded Systems
+	Author:		Andrew Davies & John Bugay
+	File:		main.c
+	Purpose:	main program code
+***********************************************************/
+
+//**Inlcudes**//
+#include "gui.h"
+#include "keypad.h"
+#include "interrupt.h"
+#include "JTAG_UART.h"
+#include "ps2.h"
+#include "isr.h"
+#include "pwm.h"
+
+//**Prototype**//
+
+//**Global Variables**//
+int ps2Interrupt;		//used in isr.c
+int encoderInterrupt;	//used in isr.c
+int monitorInterrupt;	//used in isr.c
+int goodKey;			//used in ps2.c
+char enterPress;		//used in keypad.c
+
+//**Program Code**//
+
+int main(void)
+{
+	//**Initialize Interrupts**//
+	disable_A9_interrupts();
+	set_A9_IRQ_stack();
+	config_GIC();
+	configInterupt();
+	enable_A9_interrupts();
+
+	//**Initialize GUI**//
+	GUI_Setup();
+	//**Initialize PWM**//
+	// PWM_Setup();
+
+    //GPIO
+    volatile int* GPIOPtr = (int*)0xFF200060;
+    *(GPIOPtr + 1) |= (1 << 0); // Set D0 as output
+    // *(GPIOPtr + 1) |= 0x2; // Set D1 as output
+	
+	while(1)
+	{
+		if(ps2Interrupt == 1)
+		{
+			ps2Interrupt = 0;
+          	PS2_Read();
+          	// put_jtag('A');
+          	if(goodKey == 1)
+            {
+              	
+              	Key();
+                goodKey = 0;
+            }
+		}
+		if(monitorInterrupt == 1)
+		{
+			//put_jtag('B');
+			monitorInterrupt = 0;
+		}
+		if(encoderInterrupt == 1)
+		{
+			//put_jtag('C');
+			//put_jtag('\n');
+			encoderInterrupt = 0;
+		}
+	}
+	int _end_ = 0;
+  	return _end_;
+}
+
+//**End of File**//
