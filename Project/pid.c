83d3dd934c9596d9c5d604c98650b0c5bd31da70
inedibleProgrammer <inedibleProgrammer@Gmail.com>
2018-04-30 12:19:41 -0400

    Add files via upload

--- Project/pid.c
+++ Project/pid.c
@@ -1,15 +1,2 @@
 
 
-double UpdatePID(SPid * pid, double error, double position)
-{
-	double pTerm, dTerm, iTerm;
-	pTerm = pid->pGain * error; // calculate the proportional term
-	// calculate the integral state with appropriate limiting
-	pid->iState += error;
-	if (pid->iState > pid->iMax) pid->iState = pid->iMax;
-	else if (pid->iState < pid->iMin) pid->iState = pid->iMin;
-	iTerm = pid->iGain * iState; // calculate the integral term
-	dTerm = pid->dGain * (pid->dState - position);
-	pid->dState = position;
-	return pTerm + dTerm + iTerm;
-}
--- Project/pid.h
+++ Project/pid.h
@@ -1,11 +1,6 @@
-typedef struct
-{
-	double dState; // Last position input
-	double iState; // Integrator state
-	double iMax, iMin; // Maximum and minimum allowable integrator state
-	double iGain, // integral gain
-	pGain, // proportional gain
-	dGain; // derivative gain
-} SPid;
+#ifndef PID_H_
+#define PID_H_
 
-double UpdatePID(SPid * pid, double error, double position);
+
+
+#endif /* PID_H_ */
